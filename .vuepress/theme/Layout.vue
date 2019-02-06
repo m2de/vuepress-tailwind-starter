a8209652c4be3be52340342fac77b9ddd5a8697d
Marco Mark <m2de@outlook.com>
2019-02-06 20:55:16 +0000

    UI tweaks

--- .vuepress/theme/Layout.vue
+++ .vuepress/theme/Layout.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="max-w-xl mx-auto m-8 px-8 py-4 rounded border font-sans leading-loose text-lg text-grey-darkest">
+    <div class="max-w-xl mx-auto m-8 px-8 py-4 rounded border font-sans leading-loose text-lg text-grey-darkest bg-white">
         <Content />
     </div>
 </template>
